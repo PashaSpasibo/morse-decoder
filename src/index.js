44a2ce37618b9c5294ce33d3f4234dae72d53c94
rb.nmd <pav.tro98@icloud.com>
2023-08-15 19:01:43 +0400

    task done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let symbolCode = expr.substring(i, i+10);
+        if (symbolCode === '**********'){res += ' '}
+        else{
+            let symbol = '';
+            for( let j = 0; j<symbolCode.length; j += 2){
+                if (symbolCode.substring(j,j+2) === '11'){symbol += '-'}
+                else if (symbolCode.substring(j,j+2) === '10'){symbol += '.'}
+            }
+            res += MORSE_TABLE[symbol]
+        }
+    }
+    return res
 }
 
 module.exports = {
     decode
 }
+ console.log(decode('0000101010000000101100101010110000000010**********00000011110000000010'))
